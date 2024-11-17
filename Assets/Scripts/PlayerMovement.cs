9a3e2c56fde8d50490412f73fe30815131328c89
Abi <abig4ilbrown@icloud.com>
2024-11-17 20:00:43 +0000

    hopefully this works

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,19 +9,37 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpForce = 5f;
 
     [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask ground; 
-
+    [SerializeField] LayerMask ground; // asigns layer option to the inspector 
+    [SerializeField] float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    [SerializeField] Transform playerCamera; //asigns camera to inspector 
  
 
     void Start()
     {
       rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; //it locks the curser...
+
     }
 
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //clamps vertical rotation
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //rotates camera up and down
+        transform.Rotate(Vector3.up * mouseX); //Rotate player left/right
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
+
+        //move in direction the player is facing 
+        Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput; 
+        rb.velocity = new Vector3 (moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);
+
 
         rb.velocity = new Vector3 (horizontalInput* movementSpeed, rb.velocity.y, verticalInput *movementSpeed);
 
