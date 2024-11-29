7649fc7d096aee655303ab823c419dc8a49a7973
Abi <abig4ilbrown@icloud.com>
2024-11-29 14:48:17 +0000

    gameplay's done.. basically

--- Assets/Scripts/QuickQuip.cs
+++ Assets/Scripts/QuickQuip.cs
@@ -2,24 +2,24 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TutoralTalk : MonoBehaviour
+public class QuickQuip : MonoBehaviour
 {
+    //not me adding entirely new code just to sass the player- 
 
-    // this script will be given to the "Trigger" or diolouge manager (whichever sounds cooler)
 
 
     public Text dialogueText; //how many of these do i need?
     public GameObject dialogueManager; // this will be a empty game opbject that is a child of whatever platform want to triget the dialouge 
     public GameObject dialogueBox;  //the pannel 
-    public string[] dialogueLines;  
+    public string[] dialogueLines;
     public float textSpeed = 0.05f;
-    public GameObject playerSprite;
+    public Image playerSprite;
 
     private int currentLineIndex = 0;  //i wanted fancy type writer-transition text (this just tracks the crrent line of dialogue)
     private bool isTyping = false;  // Checks if the text is still typing
     private bool dialogueActive = false;  // Checks if dialogue is active..dont know how the two are different tbh-
-    
-  
+
+
 
     void Update()
     {
@@ -41,8 +41,11 @@ public class TutoralTalk : MonoBehaviour
     }
     public void Start()
     {
-        StartDialogue();
+        dialogueBox.SetActive(false);
+        dialogueActive = false;
     }
+
+
     public void StartDialogue()
     {
         // Activate the dialogue box and start with the first line
@@ -69,7 +72,7 @@ public class TutoralTalk : MonoBehaviour
     IEnumerator TypeLine()
     {
         isTyping = true;
-        dialogueText.text = ""; 
+        dialogueText.text = "";
 
         // Type each character one by one
         foreach (char c in dialogueLines[currentLineIndex].ToCharArray())
@@ -85,16 +88,18 @@ public class TutoralTalk : MonoBehaviour
     {
         dialogueBox.SetActive(false);
         dialogueActive = false;
-        playerSprite.SetActive(false);
+        
     }
 
-   // void OnTriggerEnter(Collider other)
-   // {
-     //   if (other.gameObject.name == "Player")
-       // {
-         //   StartDialogue();
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            StartDialogue();
 
-        //}
-   // }
+        }
+    }
+  
 }
+
 
